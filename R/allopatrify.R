63e970a50732b53ea38f576733c8c1303d01625d
Dan Warren <dan.l.warren@gmail.com>
2021-10-07 14:56:36 +0900

    Switched allopatrifying algorithms
    It now uses a permutation test to try to match the distances between centroids with phylogenetic distance, so that closely related species are more likely to be closer together in space

--- R/allopatrify.R
+++ R/allopatrify.R
@@ -8,13 +8,16 @@
 #' @param alpha Alpha parameter past to convertToPA from virtual species
 #' @param env Optional stack of environmental layers, which will eventually be used to partition species based on environment as well as geography.
 #' @param npres Number of presence points per species to use for partitioning ranges.
+#' @param min.suitable Minimum number of "suitable" grid cells in a species range
+#' @param nreps Number of replicates to try for permuting ranges to match phylogeny
 #'
 #' @export allopatrify
 #'
 #' @return A vc.clade object with ranges added to the "actual range" variable for each species
 
 allopatrify <- function(x, buffer.width = 1, plot=TRUE, split.cols = c("lon", "lat"),
-                        beta = 0.5, alpha = -0.00007, env = NA, npres = 500){
+                        beta = 0.5, alpha = -0.00007, env = NA, npres = NA, min.suitable = 10,
+                        nreps = 1000){
 
    # Get the species name template from the clade object
    species.var <- gsub(".1", "", names(x$species)[1])
@@ -22,8 +25,8 @@ allopatrify <- function(x, buffer.width = 1, plot=TRUE, split.cols = c("lon", "l
    print("Getting pa data...")
 
    pa <-  clade.pa(x, sample.source = "suitab.raster",
-                   method = "probability",
-                   beta = beta, alpha = alpha, plot = plot, npres = npres)
+                   method = "probability", beta = beta,
+                   alpha = alpha, plot = plot, npres = npres)
 
    # Start building the output df
    pa.table <-  data.frame(pa$pa.table)
@@ -47,9 +50,7 @@ allopatrify <- function(x, buffer.width = 1, plot=TRUE, split.cols = c("lon", "l
       }
    }
 
-   # Get centroids of suitable habitat
-   #print(head(x.pa))
-   centroids <- plyr::ddply(pa.table[,c(species.var, split.cols)], species.var, plyr::numcolwise(mean))
+   message("allopatrifying...")
 
    # Get kmeans clusters
    unique.rows <- dplyr::distinct(pa.table, across(all_of(split.cols)), .keep_all = TRUE)
@@ -57,57 +58,96 @@ allopatrify <- function(x, buffer.width = 1, plot=TRUE, split.cols = c("lon", "l
    unique.clusters <- cbind(unique.rows, clusters$cluster)
    colnames(unique.clusters)[ncol(unique.clusters)] <- "cluster"
 
+   # Get centroids of clusters
+   centroids <- plyr::ddply(unique.clusters[,c("cluster", split.cols)], "cluster", plyr::numcolwise(mean))
+
    # This is just for a quick prototype, might want to make it more general eventually
    cluster.raster <- rasterize(unique.clusters[,c("lon", "lat")], raster.template, field=unique.clusters[,"cluster"])
-   raster::plot(cluster.raster)
+   raster::plot(cluster.raster, main = "Starting clusters")
+
+   pa.table$cluster <- extract(cluster.raster, pa.table[,c("lon", "lat")])
+
+   centroid.dist <- as.matrix(dist(centroids[,2:3], diag = TRUE, upper = TRUE))
+   phylo.dist <- cophenetic(x$tree)
+
+   suitable.cells <- pa.table %>%
+      group_by(species, cluster) %>%
+      summarise(n = n()) %>%
+      as.data.frame()
+
+   best.perm <- centroid.dist
+   max.cor <- 0
+   counter <- 1
+
+   pb <- progress::progress_bar$new(total = nreps)
+
+   while(counter < nreps){
+
+      # Getting a possible random order
+      inds <- sample(1:ncol(centroid.dist))
+
+      # Creating an empty vector to store # suitable
+      n.suitable <- numeric(length(inds))
+
+      # Counting how many suitable (presence) grid cells each species has under this solution
+      for(j in 1:length(inds)){
+         this.species <- rownames(phylo.dist)[j]
+         this.cluster <- rownames(centroid.dist)[inds[j]]
+         this.suitable <- suitable.cells %>%
+            filter(species == this.species, cluster == this.cluster) %>%
+            select(n)
+         n.suitable[j] <- as.numeric(this.suitable)
+      }
+
+      # Rejecting if any species doesn't have enough cells
+      if(any(n.suitable < min.suitable) | any(is.na(n.suitable))){
+         next
+      }
+
+      # Progress bar only ticks if we have enough suitable cells for each species
+      pb$tick()
+
+      # At this point we know the solution has enough cells per species, now we are
+      # permuting the centroid distance matrix to see how well it matches the tree
+      this.perm <- centroid.dist[inds,inds]
+      this.cor <- cor(as.numeric(this.perm), as.numeric(phylo.dist), method = "spearman")
+
+      # If this is the best solution so far we store it and update the max.cor value
+      if(this.cor > max.cor){
+         max.cor <- this.cor
+         best.perm <- this.perm
+      }
+      counter <- counter + 1
+   }
+
+   cluster.assignments <- data.frame(species = rownames(phylo.dist),
+                                     cluster = as.numeric(rownames(best.perm)))
+
+
+
+
+   ###### THIS IS WHERE I STOPPED FOR A BIT!!!
 
    # Making a unique raster for each cluster, putting them in a stack
-   for(i in unique(unique.clusters[,"cluster"])){
+   for(i in cluster.assignments$cluster){
       cluster.stack <- addLayer(cluster.stack, cluster.raster == i)
    }
    cluster.stack <- dropLayer(cluster.stack, 1)
-   names(cluster.stack) <- paste("species", unique(unique.clusters[,"cluster"]), sep = ".")
+   names(cluster.stack) <- cluster.assignments$species
 
    # Making a raster of suitable habitat for each species, putting it in a stack
-   for(i in unique(pa.table[,species.var])){
+   for(i in cluster.assignments$species){
       suit.stack <- addLayer(suit.stack,
                              rasterize(pa.table[pa.table[species.var] == i,c("lon", "lat")], suit.stack, field = 1))
    }
    suit.stack <- dropLayer(suit.stack, 1)
-   names(suit.stack) <- unique(pa.table[,species.var])
-
-   print("allopatrifying...")
-
-   # Stepping through from narrowest range to largest, putting species where they overlap with their cluster
-   temp.cluster <- cluster.stack
-   for(i in names(breadth)){
-      #print(i)
-      this.sp <- suit.stack[[i]]
-      overlaps <- c()
-      for(j in names(temp.cluster)){
-         overlaps <- c(overlaps, cellStats(this.sp * temp.cluster[[j]], stat="sum"))
-      }
-      this.pa <- this.sp * temp.cluster[[which.max(overlaps)]]
-
-      # Some species don't overlap with any remaining clusters, they get their original range back
-      if(cellStats(this.pa, sum) == 0){
-         this.pa <- this.sp
-      }
-      pa.stack[[i]] <- this.pa
-
-      # Dropping out layers that have already been assigned
-      temp.cluster <- dropLayer(temp.cluster, which.max(overlaps))
-   }
-   pa.stack <- dropLayer(pa.stack, 1)
-
-   # Convert to 1/0
-   pa.stack <- pa.stack > 0
+   names(suit.stack) <- cluster.assignments$species
 
    if(plot == TRUE){
       raster::plot(pa.stack)
    }
 
-   print("sympatrifying...")
+   message("\n\nsympatrifying...")
 
    # Here's where we're generating sympatry.  In the original sim I had this set up
    # to produce a 1 everywhere that was present in the PA raster, the true suitability
@@ -116,8 +156,8 @@ allopatrify <- function(x, buffer.width = 1, plot=TRUE, split.cols = c("lon", "l
    # for simulation purposes it might be better to have the suitability at every grid cell
    # in the range and 0 elsewhere
 
-   for(i in names(breadth)){
-      temp.pa <- pa.stack[[i]]
+   for(i in cluster.assignments$species){
+      temp.pa <- cluster.stack[[i]]
       temp.pa[temp.pa < 1 ] <- NA
       buffered.suitability <- raster::buffer(temp.pa, buffer.width) *
          x$species[[i]]$virtualspecies$suitab.raster
@@ -141,5 +181,6 @@ allopatrify <- function(x, buffer.width = 1, plot=TRUE, split.cols = c("lon", "l
       output$species[[i]]$actual.range <- sample.stack[[i]]
    }
 
+   # Might want to put more stuff into a list and return that
    return(output)
 }
